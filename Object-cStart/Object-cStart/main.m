ea308b6aed3e92549a6407829988386c3bf7b656
jzheng <zhengjin12@yahoo.com.cn>
2012-08-29 20:13:47 +0800

    add object-c project

---
+++ Object-cStart/Object-cStart/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Object-cStart
+//
+//  Created by 郑 晋 on 12-8-29.
+//  Copyright (c) 2012年 点睛工作室. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
