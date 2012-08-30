37aaed3f008a271d27ba93fcb6d0fd1eae2e746f
jzheng <zhengjin12@yahoo.com.cn>
2012-08-31 06:50:36 +0800

    binding data

--- Sqlite3Demo/Sqlite3Demo/ViewController.m
+++ Sqlite3Demo/Sqlite3Demo/ViewController.m
@@ -10,10 +10,37 @@
 
 @implementation ViewController
 
+@synthesize albumList = _albumList;
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Release any cached data, images, etc that aren't in use.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    //控制列表数量
+    // Return the number of rows in the section.
+    return 5;
+}
+
+//绑定数据
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cell_identifier = @"AlbumCell";
+    UITableViewCell *album_cell = (UITableViewCell *)[tableView dequeueReusableCellWithIdentifier:cell_identifier];
+    if (album_cell == nil) {
+        album_cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cell_identifier];
+    }
+    album_cell.textLabel.text = @"album";
+    return album_cell;
+}
+
+//单击事件
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"cell selected");
 }
 
 #pragma mark - View lifecycle
@@ -26,6 +53,7 @@
 
 - (void)viewDidUnload
 {
+    [self setAlbumList:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
