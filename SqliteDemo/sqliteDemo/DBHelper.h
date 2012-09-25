b03ea5a6631c921ee54ae5de13d66ab0e82507d2
jzheng <zhengjin12@yahoo.com.cn>
2012-09-25 22:21:31 +0800

    add dbhelp

---
+++ SqliteDemo/sqliteDemo/DBHelper.h
@@ -0,0 +1,13 @@
+//
+//  DBHelper.h
+//  sqliteDemo
+//
+//  Created by 郑 晋 on 12-9-25.
+//  Copyright (c) 2012年 点睛工作室. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBHelper : NSObject
+
+@end
---
+++ SqliteDemo/sqliteDemo/DBHelper.m
@@ -0,0 +1,13 @@
+//
+//  DBHelper.m
+//  sqliteDemo
+//
+//  Created by 郑 晋 on 12-9-25.
+//  Copyright (c) 2012年 点睛工作室. All rights reserved.
+//
+
+#import "DBHelper.h"
+
+@implementation DBHelper
+
+@end
