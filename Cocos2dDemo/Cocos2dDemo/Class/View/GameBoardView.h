0f8e1568e13d2af75b78a398231143f25183997e
jzheng <zhengjin12@yahoo.com.cn>
2012-10-16 22:22:22 +0800

    add game view class

---
+++ Cocos2dDemo/Cocos2dDemo/Class/View/GameBoardView.h
@@ -0,0 +1,18 @@
+//
+//  MyCocos2DClass.h
+//  Cocos2dDemo
+//
+//  Created by 郑 晋 on 12-10-16.
+//  Copyright 2012年 点睛工作室. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameBoardView : CCNode {
+    
+}
+
+-(id) init;
+
+@end
---
+++ Cocos2dDemo/Cocos2dDemo/Class/View/GameBoardView.m
@@ -0,0 +1,23 @@
+//
+//  MyCocos2DClass.m
+//  Cocos2dDemo
+//
+//  Created by 郑 晋 on 12-10-16.
+//  Copyright 2012年 点睛工作室. All rights reserved.
+//
+
+#import "GameBoardView.h"
+
+
+@implementation GameBoardView
+
+-(id) init{
+    if((self=[self init]))
+    {
+        //创建初始化显示层
+        CC
+    }
+    return self;
+}
+
+@end
