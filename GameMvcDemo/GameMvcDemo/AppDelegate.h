eaa488fefea886a8a2245e38d77defade95e428b
jzheng <zhengjin12@yahoo.com.cn>
2012-11-28 21:09:40 +0800

    add mvc demo project

---
+++ GameMvcDemo/GameMvcDemo/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  GameMvcDemo
+//
+//  Created by 郑 晋 on 12-11-28.
+//  Copyright 点睛工作室 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
