b63f0bc8f1979b6a0015428ec09c7c62daa7f0fa
jzheng <zhengjin12@yahoo.com.cn>
2012-12-27 20:55:23 +0800

    init ios demo

---
+++ IOSDemo/IOSDemo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  IOSDemo
+//
+//  Created by 郑 晋 on 12-12-27.
+//  Copyright (c) 2012年 点睛工作室. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
